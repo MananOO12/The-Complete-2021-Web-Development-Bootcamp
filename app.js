231b4b4bcb8c512b44d4b5bc8ad95377adb456d9
Manan Jain <manansj01@gmail.com>
2021-05-12 10:14:27 +0530

    todo configure

--- app.js
+++ app.js
@@ -152,8 +152,13 @@ app.post('/delete',function(req, res){
   }
 });
 
-app.listen(3000, function(){
-  console.log('listening on port 3000');
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function(){
+  console.log('Server Started Successfully!');
 });
 
 
